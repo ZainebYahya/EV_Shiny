3681a32e25664bd7dcbed8119e0b809ad8854a96
ZainebYahya <znbyahya@gmail.com>
2021-01-31 01:35:53 -0500

    hide the EDA part

--- global.R
+++ global.R
@@ -14,13 +14,13 @@ df <- read.csv(file = "./EV.csv")
 
 
 # Exploratory data analysis
-
-View(df)
-describe(df)
-profiling_num(df)
-freq(df)
-plot_num(df)
-glimpse(df)
+# 
+# View(df)
+# describe(df)
+# profiling_num(df)
+# freq(df)
+# plot_num(df)
+# glimpse(df)
 
 
 
@@ -28,31 +28,31 @@ glimpse(df)
 
 #Cleaning up the data
 
-
-df<- df %>% 
-  select(
-    - GPS_AVG_VELOCITY,
-    - START_ALTITUDE,
-    - END_ALTITUDE,
-    - DISTANCE_GPS,
-    - TEMPERATURE,
-    - HEADLAMP_ON_PCT,
-    - TRIP_ID,
-    - VEHICLE_ID,
-    - START_BAT_EST,
-    - END_ODO_EST,
-    - END_BAT_EST,
-    - UNACCOUNTED_KM,
-    - AC_ON_PCT)
-
-
-df <-df %>% drop_na()
-names(df) <- tolower(names(df))
-
-
-df <- df %>% 
-  mutate(.,battery_charg = (df$end_bat_actual - df$start_bat_actual))
-
-
-full_charge <- df %>% 
-  filter(fullchargeflag == 1)
+# 
+# df<- df %>% 
+#   select(
+#     - GPS_AVG_VELOCITY,
+#     - START_ALTITUDE,
+#     - END_ALTITUDE,
+#     - DISTANCE_GPS,
+#     - TEMPERATURE,
+#     - HEADLAMP_ON_PCT,
+#     - TRIP_ID,
+#     - VEHICLE_ID,
+#     - START_BAT_EST,
+#     - END_ODO_EST,
+#     - END_BAT_EST,
+#     - UNACCOUNTED_KM,
+#     - AC_ON_PCT)
+# 
+# 
+# df <-df %>% drop_na()
+# names(df) <- tolower(names(df))
+# 
+# 
+# df <- df %>% 
+#   mutate(.,battery_charg = (df$end_bat_actual - df$start_bat_actual))
+# 
+# 
+# full_charge <- df %>% 
+#   filter(fullchargeflag == 1)
